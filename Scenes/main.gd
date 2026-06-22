08996747e140d0099a3d9f66bc6008be2f7d4f09
3dgoblindev <lorenax230403@gmail.com>
2026-06-22 12:09:54 +0200

    i made a hat

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
-func _ready() -> void:
+
+func _ready():
+	# Forzar carga del bus layout
+	var bus_layout = load("res://default_bus_layout.tres")
+	if bus_layout:
+		AudioServer.set_bus_layout(bus_layout)
 	Dialogic.start("res://tls/timeline.dtl")
